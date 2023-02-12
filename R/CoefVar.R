2694f2e5596d4ab7ff366be9e0a8685dcac6c7cb
Ian Dworkin <ian.dworkin@gmail.com>
2023-02-11 22:26:07 -0500

    bunch o stuff

--- R/CoefVar.R
+++ R/CoefVar.R
@@ -1,3 +1,14 @@
+#' Computes the coefficient of variation
+#'
+#' @param x numeric vector.
+#'
+#' @return numeric vector of length 1.
+#' @export
+#'
+#' @examples
+#'
+#' x <- rnorm(100, 20, 2)
+#' CoefVar(x)
 CoefVar <- function(x) {
    CV <- sd(x)/mean(x)
    return(CV)
